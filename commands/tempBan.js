da8499494c4da503a5349d81c57578f723378312
Tycho Grandia <tychograndia53@gmail.com>
2020-03-24 23:43:34 +0100

    Update tempBan.js

--- commands/tempBan.js
+++ commands/tempBan.js
@@ -2,7 +2,15 @@
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("Hallo");
+    //  !tempban gebruiker tijd reden
+
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Sorry je hebt hier geen toestemming voor");
+
+    var user = message.guild.member(message.mentions.users.first());
+
+    if (!user) return message.channel.send("Je maakt als volgt gebruik van het commando: !tempban (gebruiker) (tijd) (reden)")
+
+    console.log(user);
 
 }
 
